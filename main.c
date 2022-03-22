aaf76ca86a28cb84cddca61f822d1b249bfc7ee9
ReneSabanagic <86081257+ReneSabanagic@users.noreply.github.com>
2022-03-22 08:52:07 +0100

    Update main.c

--- main.c
+++ main.c
@@ -316,4 +316,4 @@ void assert_failed(uint8_t *file, uint32_t line)
 }
 #endif /* USE_FULL_ASSERT */
 
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/Q	
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
